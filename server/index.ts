10e4fa57ebb7e6d8c953e973e40b99d6bded9ce9
Devansh Srivastava <devansh.srv@gmail.com>
2025-06-09 12:04:09 +0530

    init: initial commit

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: "home route" })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running over port: ${PORT}`);
+})
