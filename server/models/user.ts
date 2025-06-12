d248338fb4861b552c65e4d4b8e1d9b6e264c0c6
Devansh Srivastava <devansh.srv@gmail.com>
2025-06-12 22:29:14 +0530

    feat: signup and login pages with a TODO dashboard page

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,7 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+interface IUser extends Document {
+  username: string;
+  password: string;
+};
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+const userSchema = new mongoose.Schema<IUser>({
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 })
 
