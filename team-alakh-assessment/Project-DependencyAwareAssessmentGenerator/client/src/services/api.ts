dd6b45202764112c5953c0fbbd14ed09ba677cf1
V V SATYA PAVAN SANDEEP PENTAPATI <pspvvsandeep@gmail.com>
2025-06-14 17:12:25 +0530

    Cleanup: removed other folders, keeping only my project

--- team-alakh-assessment/Project-DependencyAwareAssessmentGenerator/client/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000', // Change to deployed URL in production
-});
-
-export default api;
--- team-alakh-assessment/Project-DependencyAwareAssessmentGenerator/server/src/models/Prerequisite.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const prereqSchema = new mongoose.Schema({
-  topic: { type: String, required: true, unique: true },
-  prerequisites: { type: [String], required: true },
-});
-
-export default mongoose.model('Prerequisite', prereqSchema);
