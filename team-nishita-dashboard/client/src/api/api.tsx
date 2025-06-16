f8b1a8572c85b0bee5be853047c9d66820633a17
Sonali <sonali291010@gmail.com>
2025-06-16 18:23:46 +0530

    added role feature

--- team-nishita-dashboard/client/src/api/api.tsx
+++ team-nishita-dashboard/client/src/api/api.tsx
@@ -1,26 +1,30 @@
 import axios from "axios";
+
 const APP_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+
 
 const api = axios.create({
   baseURL: APP_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
-})
-
+    'Content-Type': 'application/json',
+  },
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
+  if (token && config.headers) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
-})
+});
 
 export interface Credentials {
-  username: string,
-  password: string
-};
+  username: string;
+  password: string;
+  role: 'user' | 'admin';
+}
+
 export const login = (credentials: Credentials) => api.post('/login', credentials);
 export const signup = (credentials: Credentials) => api.post('/register', credentials);
+
 export default api;
